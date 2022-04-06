affa16321a310c31b60338c7d4ce3088d6f39e6a
Shreyas Naik <86342945+Saishrey@users.noreply.github.com>
2022-04-06 18:21:35 +0530

    Add files via upload

---
+++ Missionaries and Cannibals/python/missionariesANDcannibals.py
@@ -0,0 +1,157 @@
+class State:
+    def __init__(self, cannibalLeft, missionaryLeft, boatPosition, cannibalRight, missionaryRight):
+        self.cannibalLeft = cannibalLeft
+        self.missionaryLeft = missionaryLeft
+        self.boatPosition = boatPosition
+        self.cannibalRight = cannibalRight
+        self.missionaryRight = missionaryRight
+        self.parent = None
+
+    def is_goal(self):
+        return self.cannibalLeft == 0 and self.missionaryLeft == 0
+
+    def is_valid(self):
+        return self.missionaryLeft >= 0 and self.missionaryRight >= 0 \
+                and self.cannibalLeft >= 0 and self.cannibalRight >= 0 \
+                and (self.missionaryLeft == 0 or self.missionaryLeft >= self.cannibalLeft) \
+                and (self.missionaryRight == 0 or self.missionaryRight >= self.cannibalRight)
+
+
+def successors(cur_state):
+    children = []
+    if cur_state.boatPosition == 'left':
+        new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft - 2, 'right',
+                          cur_state.cannibalRight, cur_state.missionaryRight + 2)
+        ## Two missionaries cross left to right.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft - 2, cur_state.missionaryLeft, 'right',
+                          cur_state.cannibalRight + 2, cur_state.missionaryRight)
+        ## Two cannibals cross left to right.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft - 1, cur_state.missionaryLeft - 1, 'right',
+                          cur_state.cannibalRight + 1, cur_state.missionaryRight + 1)
+        ## One missionary and one cannibal cross left to right.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft - 1, 'right',
+                          cur_state.cannibalRight, cur_state.missionaryRight + 1)
+        ## One missionary crosses left to right.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft - 1, cur_state.missionaryLeft, 'right',
+                          cur_state.cannibalRight + 1, cur_state.missionaryRight)
+        ## One cannibal crosses left to right.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+    else:
+        new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft + 2, 'left',
+                          cur_state.cannibalRight, cur_state.missionaryRight - 2)
+        ## Two missionaries cross right to left.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft + 2, cur_state.missionaryLeft, 'left',
+                          cur_state.cannibalRight - 2, cur_state.missionaryRight)
+        ## Two cannibals cross right to left.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft + 1, cur_state.missionaryLeft + 1, 'left',
+                          cur_state.cannibalRight - 1, cur_state.missionaryRight - 1)
+        ## One missionary and one cannibal cross right to left.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft, cur_state.missionaryLeft + 1, 'left',
+                          cur_state.cannibalRight, cur_state.missionaryRight - 1)
+        ## One missionary crosses right to left.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+        new_state = State(cur_state.cannibalLeft + 1, cur_state.missionaryLeft, 'left',
+                          cur_state.cannibalRight - 1, cur_state.missionaryRight)
+        ## One cannibal crosses right to left.
+        if new_state.is_valid():
+            new_state.parent = cur_state
+            children.append(new_state)
+    return children
+
+
+def iterative_deepening_depth_first_search(initial_state):
+    soln = None
+    depthLevel = 0
+
+    while soln == None:
+        soln = dfs(initial_state, depthLevel)
+        depthLevel += 1
+
+    return soln
+
+
+def dfs(state, depthLevel):
+    if state.is_goal():
+        return state
+    elif depthLevel == 0:
+        return None
+    else:
+        children = successors(state)
+
+        for child in children:
+            result = dfs(child, depthLevel - 1)
+            if result:
+                return result
+    return None
+
+
+def breadth_first_search(initial_state):
+    if initial_state.is_goal():
+        return initial_state
+    frontier = list()
+    explored = set()
+    frontier.append(initial_state)
+    while frontier:
+        state = frontier.pop(0)
+        if state.is_goal():
+            return state
+        explored.add(state)
+        children = successors(state)
+        for child in children:
+            if (child not in explored) or (child not in frontier):
+                frontier.append(child)
+    return None
+
+
+def print_solution(solution):
+    path = list()
+    path.append(solution)
+    parent = solution.parent
+    while parent:
+        path.append(parent)
+        parent = parent.parent
+
+    for t in range(len(path)):
+        state = path[len(path) - t - 1]
+        print("(" + str(state.cannibalLeft) + "," + str(state.missionaryLeft) \
+        + "," + state.boatPosition + "," + str(state.cannibalRight) + "," + \
+        str(state.missionaryRight) + ")")
+
+
+print("Missionaries and Cannibals solution:")
+print("(cannibalLeft,missionaryLeft,boat,cannibalRight,missionaryRight)")
+
+initial_state = State(3, 3, 'left', 0, 0)
+
+print("Using BFS")
+solution = breadth_first_search(initial_state)
+print_solution(solution)
+
+print("\nUsing DFS")
+solution = iterative_deepening_depth_first_search(initial_state)
+print_solution(solution)
---
+++ Water Jug/python/waterJug.py
@@ -0,0 +1,132 @@
+class State:
+    def __init__(self, jug1Capacity, jug2Capacity, targetCapacity, currentJ1, currentJ2):
+        self.jug1Capacity = jug1Capacity
+        self.jug2Capacity = jug2Capacity
+        self.targetCapacity = targetCapacity
+        self.currentJ1 = currentJ1
+        self.currentJ2 = currentJ2
+        self.parent = None
+
+    def is_goal(self):
+        return self.currentJ1 == self.targetCapacity or self.currentJ2 == self.targetCapacity
+
+    def is_valid(self):
+        return 0 <= self.currentJ1 <= self.jug1Capacity \
+               and 0 <= self.currentJ2 <= self.jug2Capacity
+
+
+def successors(cur_state):
+    children = []
+
+    # Fill jug 1
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, cur_state.jug1Capacity, cur_state.currentJ2)
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    # Fill jug 2
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, cur_state.currentJ1, cur_state.jug2Capacity)
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    # Empty jug 2
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, cur_state.currentJ1, 0)
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    # Empty jug 1
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, 0, cur_state.currentJ2)
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    # Jug 1 to Jug 2
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, \
+                      max(0, cur_state.currentJ1 + cur_state.currentJ2 - cur_state.jug2Capacity), min(cur_state.currentJ1 + cur_state.currentJ2, cur_state.jug2Capacity))
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    # Jug 2 to Jug 1
+    new_state = State(cur_state.jug1Capacity, cur_state.jug2Capacity, cur_state.targetCapacity, \
+                      min(cur_state.currentJ1 + cur_state.currentJ2, cur_state.jug1Capacity), max(0, cur_state.currentJ1 + cur_state.currentJ2 - cur_state.jug1Capacity))
+    if new_state.is_valid():
+        new_state.parent = cur_state
+        children.append(new_state)
+
+    return children
+
+
+def breadth_first_search(initial_state):
+    if initial_state.is_goal():
+        return initial_state
+    frontier = list()
+    explored = set()
+    frontier.append(initial_state)
+    while frontier:
+        state = frontier.pop(0)
+        if state.is_goal():
+            return state
+        explored.add(state)
+        children = successors(state)
+        for child in children:
+            if (child not in explored) or (child not in frontier):
+                frontier.append(child)
+    return None
+
+
+def iterative_deepening_depth_first_search(initial_state):
+    soln = None
+    depthLevel = 0
+
+    while soln == None:
+        soln = dfs(initial_state, depthLevel)
+        depthLevel += 1
+
+    return soln
+
+
+def dfs(state, depthLevel):
+    if state.is_goal():
+        return state
+    elif depthLevel == 0:
+        return None
+    else:
+        children = successors(state)
+
+        for child in children:
+            result = dfs(child, depthLevel - 1)
+            if result:
+                return result
+    return None
+
+
+def print_solution(solution):
+    path = list()
+    path.append(solution)
+    parent = solution.parent
+    while parent:
+        path.append(parent)
+        parent = parent.parent
+
+    for t in range(len(path)):
+        state = path[len(path) - t - 1]
+        print("(" + str(state.currentJ1) + ", " + str(state.currentJ2) + ")")
+
+
+print("Water_Jug_Problem:")
+jug1Capacity = int(input("JUG 1 capacity: "))
+jug2Capacity = int(input("JUG 2 capacity: "))
+targetCapacity = int(input("Target capacity: "))
+
+initial_state = State(jug1Capacity, jug2Capacity, targetCapacity, 0, 0)
+
+print("Using BFS")
+solution = breadth_first_search(initial_state)
+print_solution(solution)
+
+print("\nUsing DFS")
+solution = iterative_deepening_depth_first_search(initial_state)
+print_solution(solution)
